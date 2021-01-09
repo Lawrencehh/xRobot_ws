d3b87a40fe8474397d11dd88d71aad1de1c16f51
Lawrencehh <sy160751@buaa.edu.cn>
2021-01-09 09:35:50 +0800

    在

--- src/communication_test/src/arm_auto.cpp
+++ src/communication_test/src/arm_auto.cpp
@@ -16,6 +16,7 @@ int goal_Encorder_linearModule[] = {2000,6000};
 int goal_Hall_putter_1_left[] = {3000,8000};
 int goal_Hall_putter_2_left[] = {1000,2000};
 int goal_Hall_putter_3_left[] = {5000,1500};
+#define epoch_length 2              //定义总步数，需要与goal的数组长度一致
 
 //P参数，需要在现场调参数
 const double p_Encorder_linearModule = 0.2;
@@ -47,107 +48,100 @@ public:
 	}
 	void arm_autoCallback(const communication_test::func_motors_feedback::ConstPtr & msg);
 	void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+    void auto_switch(int &epoch_switch,const communication_test::func_motors_feedback::ConstPtr & msg);
+    void motors_stop();
  
 private:
 	ros::NodeHandle nh;
 	ros::Subscriber sub;
 	ros::Subscriber sub2;
-
+    turtlebot_teleop::twist_hh func_motors; //功能电机状态
     ros::Publisher func_motors_pub_;  //所有功能电机状态发布
   
 };
  
+ //复用代码,包含epoch转换和pid中的p控制
+ void multiReceiver::auto_switch(int &epoch_switch,const communication_test::func_motors_feedback::ConstPtr & msg)
+    {
+
+        ROS_INFO_STREAM("OUTPUT>>>"<<epoch_switch);
+        //第一阶段判断是否完成
+        if(abs(msg->Encorder_linearModule - goal_Encorder_linearModule[epoch_switch]) <error_Encorder_linearModule && \
+        abs(msg->Hall_putter_1_left - goal_Hall_putter_1_left[epoch_switch]) <error_putter_1 && \
+        abs(msg->Hall_putter_2_left - goal_Hall_putter_2_left[epoch_switch]) <error_putter_2 && \
+        abs(msg->Hall_putter_3_left - goal_Hall_putter_3_left[epoch_switch]) <error_putter_3  \
+        ) epoch_switch++;
+
+        //给各个电机赋值
+        //直线模组
+        func_motors.linear_module = p_Encorder_linearModule*(goal_Encorder_linearModule[epoch_switch] - msg->Encorder_linearModule); //线性模组的前进后退
+        if(func_motors.linear_module > 100) func_motors.linear_module = 100;
+        if(func_motors.linear_module < -100) func_motors.linear_module = -100;
+        if(func_motors.linear_module > -lowest_Encorder_linearModule && func_motors.linear_module < lowest_Encorder_linearModule) func_motors.linear_module = 0;
+        //大臂
+        func_motors.putter_1 = p_putter_1*(goal_Hall_putter_1_left[epoch_switch] - msg->Hall_putter_1_left); //大臂
+        if(func_motors.putter_1 > 100) func_motors.putter_1 = 100;
+        if(func_motors.putter_1 < -100) func_motors.putter_1 = -100;
+        if(func_motors.putter_1 > -lowest_putter_1 && func_motors.putter_1 < lowest_putter_1) func_motors.putter_1 = 0;
+        
+        //小臂
+        func_motors.putter_2 = p_putter_2*(goal_Hall_putter_2_left[epoch_switch] - msg->Hall_putter_2_left); 
+        if(func_motors.putter_2 > 100)  func_motors.putter_2 = 100;
+        if(func_motors.putter_2 < -100) func_motors.putter_2 = -100;
+        if(func_motors.putter_2 > -lowest_putter_2 &&  func_motors.putter_2 < lowest_putter_2) func_motors.putter_2 = 0;
+
+        //斜板角度推杆控制
+        func_motors.oblique_angle = p_putter_3*(goal_Hall_putter_3_left[epoch_switch] - msg->Hall_putter_3_left);
+        if(func_motors.oblique_angle > 100)  func_motors.oblique_angle = 100;
+        if(func_motors.oblique_angle < -100) func_motors.oblique_angle = -100;
+        if(func_motors.oblique_angle > -lowest_putter_3 &&  func_motors.oblique_angle < lowest_putter_3) func_motors.oblique_angle = 0;
+
+
+        func_motors.oblique_drawer = 1; //斜板抽屉推杆控制
+        func_motors.flat_drawer = 0; //伸缩柜伸展控制 ×××××××××××××××××××在加装限位开关后可以改为1
+
+        func_motors.belt = 1; //输送带动作控制
+        func_motors.camera_angle = 0;  //摄像头的旋转
+        func_motors.camera_tilt = 0; //摄像头俯仰角控制
+        func_motors.arm_auto =1; //自动轨迹的使能
+        func_motors_pub_.publish(func_motors);    //发布功能电机控制话题                
+    }
+
+void multiReceiver::motors_stop()
+{
+    func_motors.linear_module = 0;
+    func_motors.putter_1 = 0;
+    func_motors.putter_2 = 0;
+    func_motors.oblique_angle = 0;
+    func_motors.oblique_drawer = 0;
+    func_motors.flat_drawer = 0;
+    func_motors.belt = 0;
+    func_motors.camera_angle = 0;
+    func_motors.camera_tilt = 0;
+    func_motors.arm_auto = 1;
+    func_motors_pub_.publish(func_motors);    //发布功能电机控制话题
+
+}
  
 void multiReceiver::arm_autoCallback(const communication_test::func_motors_feedback::ConstPtr & msg)
 {
-    turtlebot_teleop::twist_hh func_motors;
+
 
     if(auto_flag){
 
-        
-            
-
             switch (epoch)
             {
-            case 0:
-                ROS_INFO_STREAM("OUTPUT>>>"<<epoch);
-                //第一阶段判断是否完成
-                if(abs(msg->Encorder_linearModule - goal_Encorder_linearModule[epoch]) <error_Encorder_linearModule && \
-                abs(msg->Hall_putter_1_left - goal_Hall_putter_1_left[epoch]) <error_putter_1 && \
-                abs(msg->Hall_putter_2_left - goal_Hall_putter_2_left[epoch]) <error_putter_2 && \
-                abs(msg->Hall_putter_3_left - goal_Hall_putter_3_left[epoch]) <error_putter_3  \
-                ) epoch++;
-
-                //给各个电机赋值
-                //直线模组
-                func_motors.linear_module = p_Encorder_linearModule*(goal_Encorder_linearModule[epoch] - msg->Encorder_linearModule); //线性模组的前进后退
-                if(func_motors.linear_module > 100) func_motors.linear_module = 100;
-                if(func_motors.linear_module < -100) func_motors.linear_module = -100;
-                if(func_motors.linear_module > -lowest_Encorder_linearModule && func_motors.linear_module < lowest_Encorder_linearModule) func_motors.linear_module = 0;
-                //大臂
-                func_motors.putter_1 = p_putter_1*(goal_Hall_putter_1_left[epoch] - msg->Hall_putter_1_left); //大臂
-                if(func_motors.putter_1 > 100) func_motors.putter_1 = 100;
-                if(func_motors.putter_1 < -100) func_motors.putter_1 = -100;
-                if(func_motors.putter_1 > -lowest_putter_1 && func_motors.putter_1 < lowest_putter_1) func_motors.putter_1 = 0;
-                
-                //小臂
-                func_motors.putter_2 = p_putter_2*(goal_Hall_putter_2_left[epoch] - msg->Hall_putter_2_left); 
-                if(func_motors.putter_2 > 100)  func_motors.putter_2 = 100;
-                if(func_motors.putter_2 < -100) func_motors.putter_2 = -100;
-                if(func_motors.putter_2 > -lowest_putter_2 &&  func_motors.putter_2 < lowest_putter_2) func_motors.putter_2 = 0;
-
-                //斜板角度推杆控制
-                func_motors.oblique_angle = p_putter_3*(goal_Hall_putter_3_left[epoch] - msg->Hall_putter_3_left);
-                if(func_motors.oblique_angle > 100)  func_motors.oblique_angle = 100;
-                if(func_motors.oblique_angle < -100) func_motors.oblique_angle = -100;
-                if(func_motors.oblique_angle > -lowest_putter_3 &&  func_motors.oblique_angle < lowest_putter_3) func_motors.oblique_angle = 0;
-
-
-                func_motors.oblique_drawer = 1; //斜板抽屉推杆控制
-                func_motors.flat_drawer = 1; //伸缩柜伸展控制
-
-                func_motors.belt = 1; //输送带动作控制
-                func_motors.camera_angle = 0;  //摄像头的旋转
-                func_motors.camera_tilt = 0; //摄像头俯仰角控制
-                func_motors.arm_auto =1; //自动轨迹的使能
-                func_motors_pub_.publish(func_motors);    //发布功能电机控制话题                
+            default:
+                this->auto_switch(epoch,msg);
                 break;
             
-            case 1:
-                ROS_INFO_STREAM("OUTPUT>>>"<<epoch);
-                //第二阶段判断是否完成
-                if(abs(msg->Encorder_linearModule - goal_Encorder_linearModule[epoch]) <error_Encorder_linearModule && \
-                abs(msg->Hall_putter_1_left - goal_Hall_putter_1_left[epoch]) <error_putter_1 && \
-                abs(msg->Hall_putter_2_left - goal_Hall_putter_2_left[epoch]) <error_putter_2 && \
-                abs(msg->Hall_putter_3_left - goal_Hall_putter_3_left[epoch]) <error_putter_3  \
-                ) epoch++;
-
-                //给各个电机赋值
-                func_motors.linear_module = 1; //线性模组的前进后退
-                func_motors.putter_1 = 1; //大臂
-                func_motors.putter_2 = 1; //小臂
-                
-                func_motors.oblique_angle = 1; //斜板角度推杆控制
-                func_motors.oblique_drawer = 1; //斜板抽屉推杆控制
-                func_motors.flat_drawer = 1; //伸缩柜伸展控制
-
-                func_motors.belt = 1; //输送带动作控制
-                func_motors.camera_angle = 0;  //摄像头的旋转
-                func_motors.camera_tilt = 0; //摄像头俯仰角控制
-                func_motors.arm_auto =1; //自动轨迹的使能
-                func_motors_pub_.publish(func_motors);    //发布功能电机控制话题
-                break;
-            
-            case 2:
+            case epoch_length:
                 ROS_INFO_STREAM("ALL DONE");
+                this->motors_stop();
                 break;
             }
-            
         
-
-        
-
-    }
+        }
     
 }
  
